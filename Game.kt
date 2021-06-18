0df06f0e7ce34a645bc7a97b269ade1ada46d8d5
“kkiirr201314” <qq255073@gmail.com>
2021-06-18 12:31:44 +0800

    c3-5

--- Game.kt
+++ Game.kt
@@ -1,19 +1,25 @@
 fun main() {
     val name = "Madrigal"
-    var healthPoint = 80
-    var isBlessed = false
+    var healthPoints = 80
+    var isBlessed = true
+    val isImmortal = false
 
-    if (healthPoint == 100){
+    if (isBlessed && healthPoints > 50 || isImmortal){
+        println("光環")
+    } else {
+        println("空無一物")
+    }
+    if (healthPoints == 100){
         println(name + "健康狀態極佳")
-    } else if(healthPoint >= 90){
+    } else if(healthPoints >= 90){
         println(name + "有一些小擦傷")
-    } else if(healthPoint >= 75){
+    } else if(healthPoints >= 75){
         if (isBlessed){
             println(name + "雖有一些傷口，但恢復很快")
         } else{
             println(name + "有一些傷口")
         }
-    } else if(healthPoint >= 15){
+    } else if(healthPoints >= 15){
         println(name + "嚴重受傷")
     } else{
         println(name + "情況不妙")
