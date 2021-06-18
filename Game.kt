64c992212be440f0c15c92805af25eb4af309032
“kkiirr201314” <qq255073@gmail.com>
2021-06-18 13:15:22 +0800

    c3-7

--- Game.kt
+++ Game.kt
@@ -10,19 +10,15 @@ fun main() {
     } else {
         println("空無一物")
     }
-    if (healthPoints == 100){
-        println(name + "健康狀態極佳")
-    } else if(healthPoints >= 90){
-        println(name + "有一些小擦傷")
-    } else if(healthPoints >= 75){
-        if (isBlessed){
-            println(name + "雖有一些傷口，但恢復很快")
-        } else{
-            println(name + "有一些傷口")
+    val healthStatus = if (healthPoints == 100) {"健康狀態極佳"}
+    else if(healthPoints >= 90) {"有一些小擦傷"}
+    else if(healthPoints >= 75){
+        if (isBlessed) {"雖有一些傷口，但恢復很快"}
+        else {"有一些傷口"}
         }
-    } else if(healthPoints >= 15){
-        println(name + "嚴重受傷")
-    } else{
-        println(name + "情況不妙")
-    }
+    else if(healthPoints >= 15){"嚴重受傷"}
+    else{"情況不妙"}
+
+    // Player status
+    println(name + healthStatus)
 }
