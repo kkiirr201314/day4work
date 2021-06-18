955074e392ab3569caff34b6437868b61752f33d
“kkiirr201314” <qq255073@gmail.com>
2021-06-18 13:34:45 +0800

    c3-10

--- Game.kt
+++ Game.kt
@@ -8,15 +8,18 @@ fun main() {
     val auraColor = if (auraVisible) "綠色光環" else "空無一物"
     println(auraColor)
 
-    val healthStatus = if (healthPoints == 100) {"健康狀態極佳"}
-    else if(healthPoints in 90..99) {"有一些小擦傷"}
-    else if(healthPoints in 75..89){
-        if (isBlessed) {"雖有一些傷口，但恢復很快"}
-        else {"有一些傷口"}
-        }
-    else if(healthPoints >= 15){"嚴重受傷"}
-    else{"情況不妙"}
+    val healthStatus = when (healthPoints) {
+        100 -> "健康狀態極佳"
+        in 90..99 -> "有一些小擦傷"
+        in 75..89 -> if(isBlessed){
+            "雖有一些傷口，但恢復很快"
+        } else {
+        "有一些傷口"
+    }
+        in 15..74 -> "嚴重受傷"
+        else -> "情況不妙"
 
-    // Player status
-    println(name + healthStatus)
+        // Player status
+        println(name + healthStatus)
+    }
 }
