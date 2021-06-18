785d6cb4c2ffe716e0a34d2ee030e26efb52afdc
“kkiirr201314” <qq255073@gmail.com>
2021-06-18 12:16:28 +0800

    c3.1

---
+++ Game.kt
@@ -0,0 +1,10 @@
+fun main() {
+    val name = "Madrigal"
+    var healthPoint = 100
+
+    if (healthPoint == 100){
+        println(name + "健康狀態極佳")
+    } else{
+        println(name + "健康狀態不妙")
+    }
+}
