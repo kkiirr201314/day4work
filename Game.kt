401a6da1d0dbb49bc2dcd5d402a9ec6adf4d29da
“kkiirr201314” <qq255073@gmail.com>
2021-06-18 12:21:25 +0800

    c3-3

--- Game.kt
+++ Game.kt
@@ -1,10 +1,16 @@
 fun main() {
     val name = "Madrigal"
-    var healthPoint = 100
+    var healthPoint = 10
 
     if (healthPoint == 100){
         println(name + "健康狀態極佳")
+    } else if(healthPoint >= 90){
+        println(name + "有一些小擦傷")
+    } else if(healthPoint >= 75){
+        println(name + "有一些傷口")
+    } else if(healthPoint >= 15){
+        println(name + "嚴重受傷")
     } else{
-        println(name + "健康狀態不妙")
+        println(name + "情況不妙")
     }
 }
